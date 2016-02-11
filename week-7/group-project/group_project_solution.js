d3bbd8e7d0d998b158cafc577d7acfc245601475
John Maguire <jmaguire5588@gmail.com>
2016-02-10 21:41:22 -0500

    Add work for group project

---
+++ week-7/group-project/group_project_solution.js
@@ -0,0 +1,10 @@
+//Person 1: User Stories
+//1) As a user I want to make sure that sum is a defined function.
+//2) As a user I want to check to make sure that the sum of the oddLengthArray is 27.
+//3) As a user I want to check that the sum of the evenLengthArray is 43
+//4) As a user I want to make sure that mean is a defined function.
+//5) As a user I want to make sure that the average of the oddLengthArray is 3.857142857142857
+//6) As a user I want to make sure that the average of the evenLengthArray is 5.375
+//7) As a user I want to check that median is a function.
+//8) As a user I want to check that the median value of oddLengthArray is 4.
+//9) As a user I want to check that the median value of evenLengthArray is 5.5.
